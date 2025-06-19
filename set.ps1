14ca685cc7d93bd655e9ab307e7c35dc5a277280
jalkn <alejalkndro@gmail.com>
2025-06-19 09:15:09 -0500

    Initial commit

---
+++ set.ps1
@@ -0,0 +1,28 @@
+function arpa {
+    param (
+        [string]$ExcelFilePath = $null
+    )
+
+    $YELLOW = [ConsoleColor]::Yellow
+    $GREEN = [ConsoleColor]::Green
+
+    Write-Host "🚀 Creating ARPA" -ForegroundColor $YELLOW
+
+    # Create Python virtual environment
+    python -m venv .venv
+    .\.venv\scripts\activate
+
+    # Install required Python packages
+    python.exe -m pip install --upgrade pip
+    python -m pip install django whitenoise django-bootstrap-v5 openpyxl pandas xlrd>=2.0.1 pdfplumber fitz
+
+    # Create Django project
+    django-admin startproject arpa
+    cd arpa
+
+    # Create core app
+    python manage.py startapp core
+
+}
+
+arpa
